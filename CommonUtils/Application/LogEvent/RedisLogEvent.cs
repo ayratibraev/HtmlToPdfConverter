081a58e9e8b03aacaad64729b2cfeab05f60438d
ayratibraev <ibraev.ayrat@yandex.ru>
2022-10-09 11:30:41 +0600

    Fix. Add forgotten files

---
+++ CommonUtils/Application/LogEvent/RedisLogEvent.cs
@@ -0,0 +1,8 @@
+namespace CommonUtils.Application.LogEvent;
+
+public static class RedisLogEvent
+{
+    public const int Connect = 1000;
+    public const int Upload = 1001;
+    public const int Download = 1002;
+}
